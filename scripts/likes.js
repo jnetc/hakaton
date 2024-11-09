905ab693469e9c7e1c536dd02dafb162673e7d0a
Anton <frontendevs@gmail.com>
2024-11-09 22:43:58 +0200

    updated likes and remove css

--- scripts/likes.js
+++ scripts/likes.js
@@ -1,22 +1,18 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', () => {
     const likeBtns = document.querySelectorAll('.btn-like');
-    console.log(likeBtns);
     for (const btn of likeBtns) {
         btn.addEventListener('click', () => {
             const likesCount = btn.querySelector('.likes-count');
-            let currentLikes = Number(likesCount.dataset.likes);
-            console.log(currentLikes++);
-            likesCount.setAttribute('data-likes', `${currentLikes++}`);
-            likesCount.textContent = formatNumber(currentLikes);
+            const currentLikes = Number(likesCount.dataset.likes) + 1;
+            likesCount.setAttribute('data-likes', `${currentLikes}`);
+            if (currentLikes >= 1000) {
+                likesCount.textContent = `${(currentLikes / 1000).toFixed(1)}k`;
+            }
+            else {
+                likesCount.textContent = `${currentLikes}`;
+            }
         });
     }
-    const formatNumber = (number) => {
-        if (number < 1000)
-            return number.toString();
-        if (number < 1000000)
-            return `${(number / 1000).toFixed(1)}k`;
-        return `${(number / 1000000).toFixed(1)}M`;
-    };
 });
 //# sourceMappingURL=likes.js.map
--- src/likes.ts
+++ src/likes.ts
@@ -1,23 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const likeBtns = document.querySelectorAll('.btn-like');
 
-  console.log(likeBtns);
   for (const btn of likeBtns) {
     btn.addEventListener('click', () => {
       const likesCount = btn.querySelector('.likes-count') as HTMLSpanElement;
 
-      let currentLikes = Number(likesCount.dataset.likes);
-      console.log(currentLikes++);
-      likesCount.setAttribute('data-likes', `${currentLikes++}`);
+      const currentLikes = Number(likesCount.dataset.likes) + 1;
+      likesCount.setAttribute('data-likes', `${currentLikes}`);
 
-      likesCount.textContent = formatNumber(currentLikes);
+      if (currentLikes >= 1000) {
+        likesCount.textContent = `${(currentLikes / 1000).toFixed(1)}k`;
+      } else {
+        likesCount.textContent = `${currentLikes}`;
+      }
     });
   }
-
-  const formatNumber = (number: number) => {
-    if (number < 1000) return number.toString();
-    if (number < 1_000_000) return `${(number / 1000).toFixed(1)}k`;
-
-    return `${(number / 1_000_000).toFixed(1)}M`;
-  };
 });
